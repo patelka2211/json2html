51401f17392e9113138834db39458da6cb9be9a3
Kartavya Patel <82671701+patelka2211@users.noreply.github.com>
2022-10-28 13:15:43 +0530

    ➕ Added some more functionalities.

--- source/json2html.js
+++ source/json2html.js
@@ -1,7 +1,15 @@
+/**
+ * Helps to create HTML in JavaScript. Uses JSON to store attributes and elements.
+ */
 export default class json2html {
-    constructor() {}
+    constructor() {
+        this.list = [];
+    }
 
-    get_attrs(attrs_obj) {
+    /**
+     * Creates string of attributes from attributes object.
+     */
+    #get_attrs(attrs_obj) {
         let output = "",
             attrs_array = Object.entries(attrs_obj);
 
@@ -12,25 +20,138 @@ export default class json2html {
         return output;
     }
 
-    convert(input) {
-        let output = "",
-            array = Object.entries(input);
+    /**
+     * Takes element and appends to root element.
+     * @param element Takes object (e.g. this.list)
+     * @returns Object itself.
+     */
+    add(element) {
+        this.list.push(element);
+        return this;
+    }
 
-        for (let index = 0; index < array.length; index++) {
-            const element = array[index];
-            if (element[1].length == 2) {
-                if (typeof element[1][1] == "object") {
-                    output += `<${element[0]} ${this.get_attrs(
-                        element[1][0]
-                    )}> ${this.convert(element[1][1])} </${element[0]}>`;
+    /**
+     * Converts JSON to HTML.
+     * @param input Takes object (e.g. this.list) (Default)
+     * @returns HTML string.
+     */
+    get_html(input = this.list) {
+        let output = "";
+
+        if (typeof input == "object" && input.length == undefined) {
+            let array = Object.entries(input);
+            for (let index = 0; index < array.length; index++) {
+                const element = array[index];
+                if (element[1].length == 2) {
+                    if (typeof element[1][1] == "object") {
+                        output += `<${element[0]} ${this.#get_attrs(
+                            element[1][0]
+                        )}>${this.get_html(element[1][1])}</${element[0]}>`;
+                    } else
+                        output += `<${element[0]} ${this.#get_attrs(
+                            element[1][0]
+                        )}>${element[1][1]}</${element[0]}>`;
                 } else
-                    output += `<${element[0]} ${this.get_attrs(
+                    output += `<${element[0]} ${this.#get_attrs(
                         element[1][0]
-                    )}> ${element[1][1]} </${element[0]}>`;
-            } else
-                output += `<${element[0]} ${this.get_attrs(element[1][0])}/>`;
+                    )}/>`;
+            }
+
+            return output;
+        }
+
+        for (let index = 0; index < input.length; index++) {
+            output += this.get_html(input[index]);
         }
 
         return output;
+    }
+
+    /**
+     * Repeats given input.
+     * @param what_to_repeat Specify what to repeat.
+     * @param how_many_times Specity how many times to repeat.
+     * @returns List of given input.
+     */
+    repeater(what_to_repeat, how_many_times) {
+        let output = [];
+        for (let index = 0; index < how_many_times; index++) {
+            output.push(what_to_repeat);
+        }
+        return output;
+    }
+
+    /**
+     * '\<div\>' tag.
+     * @param attributes Can be object or list of objects.
+     * @param innerHTML Can be object/string or list of objects/strings.
+     * @returns '\<div\>' tag.
+     */
+    div(attributes = {}, innerHTML = "") {
+        if (typeof attributes == "object" && attributes.length != undefined) {
+            let output = [];
+
+            for (let index = 0; index < attributes.length; index++) {
+                try {
+                    output.push({ div: [attributes[index], innerHTML[index]] });
+                } catch (error) {
+                    alert(error);
+                }
+            }
+            return output;
+        }
+        return { div: [attributes, innerHTML] };
+    }
+
+    /**
+     * '\<a\>' tag.
+     * @param href Can be string or list of strings.
+     * @param innerHTML Can be object/string or list of objects/strings.
+     * @param attributes Can be object or list of objects.
+     * @returns '\<a\>' tag.
+     */
+    a(href = "#", innerHTML = "", attributes = {}) {
+        if (typeof href == "object" && href.length != undefined) {
+            let output = [];
+
+            for (let index = 0; index < href.length; index++) {
+                try {
+                    attributes[index].href = href[index];
+                    output.push({ div: [attributes[index], innerHTML[index]] });
+                } catch (error) {
+                    alert(error);
+                }
+            }
+            return output;
+        }
+        attributes.href = href;
+        return { a: [attributes, innerHTML] };
+    }
+
+    /**
+     * '\<img\>' tag.
+     * @param src Can be string or list of strings.
+     * @param alt Can be string or list of strings.
+     * @param attributes Can be object or list of objects.
+     * @returns '\<img\>' tag.
+     */
+    img(src = "", alt = "", attributes = {}) {
+        if (typeof src == "object" && src.length != undefined) {
+            let output = [];
+
+            for (let index = 0; index < src.length; index++) {
+                try {
+                    attributes[index].src = src[index];
+                    attributes[index].alt = alt[index];
+                    output.push({ div: [attributes[index], innerHTML[index]] });
+                } catch (error) {
+                    alert(error);
+                }
+            }
+            return output;
+        }
+        attributes.src = src;
+        attributes.alt = alt;
+        return { img: [attributes] };
     }
 }
--- supporting-code/min-js-generator.py
+++ supporting-code/min-js-generator.py
@@ -2,8 +2,43 @@ input_file_path = '../source/json2html.js'
 output_file_path = '../source/json2html.min.js'
 
 with open(input_file_path, 'r') as file:
-    code = file.read()
-    code = ' '.join(code.replace('\n', '').split())
+
+    lines = file.readlines()
+
+    def has_starting_of_multiline_comment(string):
+        if string.find('/*') == -1:
+            return False
+        return True
+
+    def has_ending_of_multiline_comment(string):
+        if string.find('*/') == -1:
+            return False
+        return True
+
+    def has_single_line_comment(string):
+        if string.find('//') == -1:
+            return False
+        return True
+
+    dont_count = False
+
+    for index, line in enumerate(lines):
+        if has_starting_of_multiline_comment(lines[index]) and has_ending_of_multiline_comment(lines[index]):
+            lines[index] = lines[index][:lines[index].find(
+                '/*')]+lines[index][lines[index].find('*/')+2:]
+        if has_starting_of_multiline_comment(lines[index]) and not has_ending_of_multiline_comment(lines[index]):
+            lines[index] = lines[index][:lines[index].find('/*')]
+            dont_count = True
+        if not has_starting_of_multiline_comment(lines[index]) and has_ending_of_multiline_comment(lines[index]):
+            lines[index] = lines[index][lines[index].find('*/')+2:]
+            dont_count = False
+
+        if dont_count:
+            lines[index] = ''
+        else:
+            if has_single_line_comment(lines[index]):
+                lines[index] = lines[index][:lines[index].find('//')]
+    lines = ' '.join(''.join(lines).replace('\n', ' ').split())
 
 with open(output_file_path, 'w') as file:
-    file.write(code)
+    file.write(lines)
